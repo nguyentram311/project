ba86d82393a3649fab8de8e261110216ac038550
nguyentram311 <nguyentram311@gmail.com>
2016-04-16 11:18:48 +0700

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -9,18 +9,4 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-#bài 1
-# @app.route('/index')
-# def index():
-#     return render_template('index.html',article=List_object[0])
 
-#bài 2
-#dùng for list ở html ko cần dùng ở hàm def nữa
-@app.route('/index')
-def index():
-    return render_template('index2.html',htmt_list=List_object)
-if __name__ == '__main__':
-    app.run()
-
-
-#dùng for list ở html ko cần dùng ở hàm def nữa
